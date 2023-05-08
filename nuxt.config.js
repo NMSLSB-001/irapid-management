25a40b09ceff4753a9c8d2f80bd9b70c54806a96
zaofei <amadashunli@hotmail.com>
2023-05-09 03:27:33 +0800

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,9 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['ant-design-vue/dist/antd.css', 'element-ui/lib/theme-chalk/index.css'],
 
+  router: {
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/antd-ui', '@/plugins/request', '@/plugins/element-ui'],
 
